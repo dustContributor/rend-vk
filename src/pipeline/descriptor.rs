cd056a620e664d6f57b5250eaf09d68b3d44d6a0
dustContributor <hctf90@gmail.com>
2023-05-07 19:19:07 -0300

    - Added a few methods to fetch and place descriptors in one go.

--- src/pipeline/descriptor.rs
+++ src/pipeline/descriptor.rs
@@ -89,6 +89,68 @@ impl DescriptorBuffer {
         self.host[offset..(offset + self.descriptor_size)].copy_from_slice(data);
     }
 
+    pub fn place_image_at(
+        &mut self,
+        index: u32,
+        desc: vk::DescriptorImageInfo,
+        desc_buffer_instance: ash::extensions::ext::DescriptorBuffer,
+    ) {
+        self.get_desc_and_place(
+            index,
+            desc,
+            vk::DescriptorType::SAMPLED_IMAGE,
+            desc_buffer_instance,
+            |e| vk::DescriptorDataEXT { p_sampled_image: e },
+        )
+    }
+
+    pub fn place_ubo_at(
+        &mut self,
+        index: u32,
+        desc: vk::DescriptorAddressInfoEXT,
+        desc_buffer_instance: ash::extensions::ext::DescriptorBuffer,
+    ) {
+        self.get_desc_and_place(
+            index,
+            desc,
+            vk::DescriptorType::UNIFORM_BUFFER,
+            desc_buffer_instance,
+            |e| vk::DescriptorDataEXT {
+                p_uniform_buffer: e,
+            },
+        )
+    }
+
+    fn get_desc_and_place<T, F>(
+        &mut self,
+        index: u32,
+        desc: T,
+        desc_type: vk::DescriptorType,
+        desc_buffer_instance: ash::extensions::ext::DescriptorBuffer,
+        info_of: F,
+    ) where
+        F: FnOnce(*const T) -> vk::DescriptorDataEXT,
+    {
+        assert!(
+            desc_type == self.descriptor_type,
+            "Can't place a {:?} on a {:?} buffer!",
+            desc_type,
+            self.descriptor_type
+        );
+        let p_desc = &desc as *const T;
+        let info_data = info_of(p_desc);
+        let info = vk::DescriptorGetInfoEXT {
+            ty: desc_type,
+            data: info_data,
+            ..Default::default()
+        };
+        let mut data = vec![0; self.descriptor_size];
+        unsafe {
+            desc_buffer_instance.get_descriptor(&info, &mut data);
+        }
+        self.place_at(index, &data)
+    }
+
     pub fn into_device(&mut self) {
         let mut device_buffer_slice = unsafe {
             ash::util::Align::new(
