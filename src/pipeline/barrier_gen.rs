3a6667d227d6a6e1c69722f773e48165691eeb00
dustContributor <hctf90@gmail.com>
2023-12-31 23:02:56 -0300

    - Initial pass at transitions including blitting stages

--- src/pipeline/barrier_gen.rs
+++ src/pipeline/barrier_gen.rs
@@ -54,6 +54,7 @@ impl BarrierGen {
     ) -> Vec<vk::ImageMemoryBarrier2> {
         let mut i = currenti;
         let mut barriers: Vec<vk::ImageMemoryBarrier2> = Vec::new();
+        let curr_is_blitting = self.passes[currenti].is_blitting;
         fn wrap_around(index: usize, length: usize) -> usize {
             if index == 0 {
                 length - 1
@@ -72,20 +73,52 @@ impl BarrierGen {
                     break;
                 }
                 let prev = &self.passes[i];
+                let old_layout;
+                let src_access;
                 if prev.inputs.iter().any(|e| e.eq(&input.name)) {
-                    // Already issued barrier before
-                    break;
-                } else if !prev.outputs.contains(&input.name) {
-                    // Continue to previous pass
+                    // Previous pass had this attachment as an input
+                    if (prev.is_blitting && curr_is_blitting)
+                        || (!prev.is_blitting && !curr_is_blitting)
+                    {
+                        // Already issued this same barrier before
+                        break;
+                    }
+                    if prev.is_blitting {
+                        // Curr is not blitting, prev was. Issue a transition from transfer src
+                        old_layout = vk::ImageLayout::TRANSFER_SRC_OPTIMAL;
+                        src_access = vk::AccessFlags2::MEMORY_READ;
+                    } else {
+                        // Curr is blitting, prev wasn't. Issue a transition from shader read
+                        old_layout = vk::ImageLayout::READ_ONLY_OPTIMAL;
+                        src_access = vk::AccessFlags2::MEMORY_READ;
+                    }
+                } else if prev.outputs.contains(&input.name) {
+                    // Previous pass had this attachment as an output
+                    if prev.is_blitting {
+                        // Prev was blitting. Issue a transition from transfer dst
+                        old_layout = vk::ImageLayout::TRANSFER_DST_OPTIMAL;
+                        src_access = vk::AccessFlags2::MEMORY_WRITE;
+                    } else {
+                        // Prev wasn't blitting. Issue a transition from fragment shader write
+                        old_layout = vk::ImageLayout::ATTACHMENT_OPTIMAL;
+                        src_access = vk::AccessFlags2::MEMORY_WRITE;
+                    }
+                } else {
+                    // Previous pass didn't reference this attachment, continue.
                     continue;
                 }
                 // Image was written to before, barrier for reading
+                let new_layout = if curr_is_blitting {
+                    vk::ImageLayout::TRANSFER_SRC_OPTIMAL
+                } else {
+                    vk::ImageLayout::READ_ONLY_OPTIMAL
+                };
                 let barrier = vk::ImageMemoryBarrier2::builder()
                     .image(input.image)
-                    .src_access_mask(vk::AccessFlags2::MEMORY_WRITE)
+                    .src_access_mask(src_access)
                     .dst_access_mask(vk::AccessFlags2::MEMORY_READ)
-                    .old_layout(vk::ImageLayout::ATTACHMENT_OPTIMAL)
-                    .new_layout(vk::ImageLayout::READ_ONLY_OPTIMAL)
+                    .old_layout(old_layout)
+                    .new_layout(new_layout)
                     .src_stage_mask(vk::PipelineStageFlags2::COLOR_ATTACHMENT_OUTPUT)
                     .dst_stage_mask(vk::PipelineStageFlags2::FRAGMENT_SHADER)
                     .subresource_range(Attachment::default_subresource_range(input.format.aspect()))
@@ -109,20 +142,52 @@ impl BarrierGen {
                     break;
                 }
                 let prev = &self.passes[i];
+                let old_layout;
+                let src_access;
                 if prev.outputs.contains(&output.name) {
-                    // Already issued barrier before
-                    break;
-                } else if !prev.inputs.iter().any(|e| e.eq(&output.name)) {
+                    // Previous pass had this attachment as an output
+                    if (prev.is_blitting && curr_is_blitting)
+                        || (!prev.is_blitting && !curr_is_blitting)
+                    {
+                        // Already issued this same barrier before
+                        break;
+                    }
+                    if prev.is_blitting {
+                        // Curr is not blitting, prev was. Issue a transition from transfer dst
+                        old_layout = vk::ImageLayout::TRANSFER_DST_OPTIMAL;
+                        src_access = vk::AccessFlags2::MEMORY_WRITE;
+                    } else {
+                        // Curr is blitting, prev wasn't. Issue a transition from shader write
+                        old_layout = vk::ImageLayout::ATTACHMENT_OPTIMAL;
+                        src_access = vk::AccessFlags2::MEMORY_WRITE;
+                    }
+                } else if prev.inputs.iter().any(|e| e.eq(&output.name)) {
+                    // Previous pass had this attachment as an input
+                    if prev.is_blitting {
+                        // Prev was blitting. Issue a transition from transfer dst
+                        old_layout = vk::ImageLayout::TRANSFER_SRC_OPTIMAL;
+                        src_access = vk::AccessFlags2::MEMORY_READ;
+                    } else {
+                        // Prev wasn't blitting. Issue a transition from fragment shader read
+                        old_layout = vk::ImageLayout::READ_ONLY_OPTIMAL;
+                        src_access = vk::AccessFlags2::MEMORY_READ;
+                    }
+                } else {
                     // Continue to previous pass
                     continue;
                 }
                 // Image was read before, issue barrier for writing
+                let new_layout = if curr_is_blitting {
+                    vk::ImageLayout::TRANSFER_DST_OPTIMAL
+                } else {
+                    vk::ImageLayout::ATTACHMENT_OPTIMAL
+                };
                 let barrier = vk::ImageMemoryBarrier2::builder()
                     .image(output.image)
-                    .src_access_mask(vk::AccessFlags2::MEMORY_READ)
+                    .src_access_mask(src_access)
                     .dst_access_mask(vk::AccessFlags2::MEMORY_WRITE)
-                    .old_layout(vk::ImageLayout::UNDEFINED)
-                    .new_layout(vk::ImageLayout::ATTACHMENT_OPTIMAL)
+                    .old_layout(old_layout)
+                    .new_layout(new_layout)
                     .src_stage_mask(vk::PipelineStageFlags2::NONE)
                     .dst_stage_mask(if output.format.has_depth_or_stencil() {
                         vk::PipelineStageFlags2::EARLY_FRAGMENT_TESTS
